39cf18afe8becfba8b00ddc6f0b59b89511daf71
Thanawat2002 <thanawat.220222@gmail.com>
2024-09-14 12:53:40 +0700

    [Update] ui calculator and fix duplicate class tailwind

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -4,34 +4,65 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='bg-blue-400 flex flex-col justify-center h-screen w-full'>
-      <div className='bg-green-400 flex justify-end'>
-        Setting
+    <div className='bg-[#3C3D37] flex flex-col justify-center h-screen w-full'>
+      <div className='bg-black flex justify-end items-center h-[5vh]'>
+        <p className='mr-[1rem] text-white text-2xl'>Setting</p>
       </div>
-      <div className='bg-red-500 flex flex-col items-end'>
-        <div>10+20</div>
-        <div>30</div>
+      <div className='flex flex-col items-end justify-center h-[45vh]'>
+        <div className='text-5xl text-white mr-[1rem]'>10+20</div>
+        <div className='text-7xl text-white font-bold mt-[5vh] mr-[1rem]'>100,000</div>
       </div>
-      <div className='bg-pink-500 grid grid-cols-4 content-center'>
-        <div className='bg-white border text-center w-[5rem] h-[5rem]'>+</div>
-        <div className='bg-white border text-center w-[5rem] h-[5rem]'>x</div>
-        <div className='bg-white border text-center w-[5rem] h-[5rem]'>DEL</div>
-        <div className='bg-white border text-center w-[5rem] h-[5rem]'>AC</div>
+      <div className='grid grid-cols-4 place-items-center gap-2'>
+        <button className='btn-operator'>
+          +
+        </button>
+        <button className='btn-operator'>
+          x
+        </button>
+        <button className='bg-[#ECDFCC] text-center text-3xl w-[80%] h-[8vh] rounded-lg mt-[1rem] mb-[1rem]'>
+          DEL
+        </button>
+        <button className='bg-[#D45555] text-center text-white text-3xl w-[80%] h-[8vh] rounded-lg mt-[1rem] mb-[1rem]'>
+          AC
+        </button>
       </div>
-      <div className='bg-purple-500 grid grid-cols-3'>
-        <div className='bg-white border text-center'>7</div>
-        <div className='bg-white border text-center'>8</div>
-        <div className='bg-white border text-center'>9</div>
-        <div className='bg-white border text-center'>4</div>
-        <div className='bg-white border text-center'>5</div>
-        <div className='bg-white border text-center'>6</div>
-        <div className='bg-white border text-center'>1</div>
-        <div className='bg-white border text-center'>2</div>
-        <div className='bg-white border text-center'>3</div>
+      
+      <div className=' grid grid-cols-3 place-items-center gap-2'>
+        <button className='btn-number'>
+          7
+        </button>
+        <button className='btn-number'>
+          8
+        </button>
+        <button className='btn-number'>
+          9
+        </button>
+        <button className='btn-number'>
+          4
+        </button>
+        <button className='btn-number'>
+          5
+        </button>
+        <button className='btn-number'>
+          6
+        </button>
+        <button className='btn-number'>
+          1
+        </button>
+        <button className='btn-number'>
+          2
+        </button>
+        <button className='btn-number'>
+          3
+        </button>
       </div>
-      <div className='bg-cyan-500 grid grid-cols-2'>
-        <div className='bg-white border text-center'>0</div>
-        <div className='bg-white border text-center'>QR</div>
+      <div className=' grid grid-cols-3 place-items-center'>
+        <button className='btn-number'>
+          0
+        </button>
+        <button className='bg-[#52913D] text-center text-5xl text-white font-bold w-[80%] h-[8vh] rounded-lg mt-[1rem] mb-[1rem] col-span-2'>
+          QR
+        </button>
       </div>
     </div>
   )

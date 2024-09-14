02769470502932682d12d7f58fe15bc433172d7d
Thanawat2002 <thanawat.220222@gmail.com>
2024-09-14 12:53:40 +0700

    [Update] ui calculator and fix duplicate class tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,51 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.btn-number': {
+          backgroundColor: '#697565',
+          color: 'white',
+          textAlign: 'center',
+          fontSize: '3rem',
+          fontWeight: 'bold',
+          width: '80%',
+          height: '8vh',
+          borderRadius: '0.5rem',
+          marginTop: '1rem',
+          marginBottom: '1rem',
+        },
+        '.btn-operator': {
+          backgroundColor: '#ECDFCC',
+          color: 'black',
+          textAlign: 'center',
+          fontSize: '3rem',
+          width: '80%',
+          height: '8vh',
+          borderRadius: '0.5rem',
+          marginTop: '1rem',
+          marginBottom: '1rem',
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+  ],
 };
+
 export default config;
